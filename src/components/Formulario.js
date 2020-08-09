252358ce2db8a3fb4ceeb03a99197a46a42872e5
leomoyano <leomoyano7@gmail.com>
2020-08-09 12:07:05 -0300

    update 9/8

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Formulario = () => {
+
+    const [cita, setCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+
+    })
+
+
+    //Para actualizar estado de inputs
+    const handleChange = () => {
+        console.log('Escribiendo...');
+    }
+
     return (
         <Fragment>
             <h2>Crear cita</h2>
@@ -12,30 +28,35 @@ const Formulario = () => {
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre de Mascota"
+                    onChange={handleChange}
                 />
                 <label htmlFor="">Nombre Dueño</label>
                 <input
                     type="text"
-                    name="mascota"
+                    name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del Dueño de Mascota"
+                    onChange={handleChange}
                 />
                 <label htmlFor="">Fecha</label>
                 <input
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={handleChange}
                 />
                 <label htmlFor="">Hora</label>
                 <input
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={handleChange}
                 />
                 <label htmlFor="">Sintomas</label>
                 <textarea
                     className="u-full-width"
                     name="sintomas"
+                    onChange={handleChange}
                 ></textarea>
                 <button
                     type="submit"
